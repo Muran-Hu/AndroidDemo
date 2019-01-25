bb01f2a421fbc21677f42967d888e2605c982323
Muran-Hu <muranhu@gmail.com>
2019-01-25 11:39:37 +0800

    import RxJava, RxAndroid

--- Rx/src/main/java/com/droid/rx/MainActivity.java
+++ Rx/src/main/java/com/droid/rx/MainActivity.java
@@ -3,11 +3,47 @@ package com.droid.rx;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
+
 public class MainActivity extends AppCompatActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    Observable<Integer> observable = Observable.create(new ObservableOnSubscribe<Integer>() {
+      @Override
+      public void subscribe(ObservableEmitter<Integer> emitter) throws Exception {
+        emitter.onNext(1);
+      }
+    });
+
+    observable.subscribe(new Observer<Integer>() {
+      @Override
+      public void onSubscribe(Disposable d) {
+
+      }
+
+      @Override
+      public void onNext(Integer integer) {
+        System.out.println("onNext => " + integer);
+      }
+
+      @Override
+      public void onError(Throwable e) {
+
+      }
+
+      @Override
+      public void onComplete() {
+
+      }
+    });
+
   }
 }
